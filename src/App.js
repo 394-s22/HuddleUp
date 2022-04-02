435f3aa8bde5dc6ab8cc2c6440e07aabc1b4b553
AmoghBharadwaj2 <102871499+AmoghBharadwaj2@users.noreply.github.com>
2022-04-02 14:55:53 -0500

    Co-authored-by: jamie7533 <jamie7533@users.noreply.github.com> Co-authored-by: Angel <angelehernandez@users.noreply.github.com> Co-authored-by: Wingchi Tam <wingchi-tam@users.noreply.github.com> Co-authored-by: Yousef Farge <yfarge@users.noreply.github.com> Co-authored-by: David-Zhang-DZ <David-Zhang-DZ@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ const Event = ({ event }) => (
   //   { event.title } hosted by { event.host } at { event.location }
   //   { event.date } : {event.start_time}-{event.end_time}
   // </div>
-  <Card style={{ width: '20rem' }}>
+  <Card style={{ textAlign: 'left' }}>
   <Card.Body>
     <Card.Title>{ event.title }</Card.Title>
     <Card.Subtitle className="mb-2 text-muted">Host: { event.host } </Card.Subtitle>
     <Card.Subtitle className="mb-2 text-muted">Location: { event.location } </Card.Subtitle>
-    <Card.Subtitle className="mb-2 text-muted">{ event.date }: { event.start_time } - { event.end_time } </Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted">Minimum Players: { event.min_players } </Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted">
+      Players Signed Up: { event.current_players }/{ event.max_players } 
+      </Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted">
+      Date: { event.date } : { event.start_time } - { event.end_time } 
+      </Card.Subtitle>
     <Card.Text>
       {event.description}
     </Card.Text>
@@ -27,14 +33,14 @@ const Event = ({ event }) => (
 );
 
 const EventsList = ({ events }) => (
-  <div>
+  <Stack direction="vertical" gap={3}>
   { Object.values(events).map(event => <Event key={ event.id } event={ event } />) }
-  </div>
+  </Stack>
 );
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
       <Banner title={ fakeEvents.title } />
       <EventsList events={ fakeEvents.events } />
   </div>
