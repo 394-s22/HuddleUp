4c8c80646284707b59bb3424ec7fe8cf08dfc9ba
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-04-02 16:59:09 -0500

    modularize the code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Stack, Button } from 'react-bootstrap';
 import fakeEvents from './fakeEvents.json';
 import EventsList from './components/EventsList';
+import SearchBar from './components/SearchBar';
 import { useState, useEffect } from 'react';
 
 const Banner = ({ title }) => (
@@ -10,47 +11,14 @@ const Banner = ({ title }) => (
 );
 
 const App = () => {
-  const [searchText, setSearchText] = useState('');
-  const [eventsList, setEventsList] = useState(fakeEvents.events)
-
-  const handleSearch = () => {
-    //use searchText to filter cards
-    console.log(searchText);
-    setEventsList(fakeEvents.events.filter(event => 
-      (event.title + 
-        event.description + 
-        event.sport + 
-        event.host + 
-        event.location).toLowerCase().includes(searchText.toLowerCase())));
-  }
-
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") handleSearch();
-  }
+  const [eventsList, setEventsList] = useState(fakeEvents.events);
+  const [joinedEvents, setJoinedEvents] = useState([]);
 
   return (
     <div className='App'>
       <Banner title={fakeEvents.title} />
-      <Stack direction="horizontal" gap={3}>
-        <Form.Control
-          className="me-auto"
-          placeholder="Search events..."
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyPress = {handleKeyPress}
-        />
-        <Button
-          variant="secondary"
-          onClick={handleSearch}>
-          Search
-        </Button>
-        <div className="vr" />
-        <Button
-          variant="outline-danger"
-          onClick={() => setEventsList(fakeEvents.events)}>
-          Reset
-        </Button>
-      </Stack>
-      <EventsList events={eventsList} />
+      <SearchBar fakeEvents={fakeEvents} setEventsList={setEventsList}/>
+      <EventsList events={eventsList} setEventsList={setEventsList}/>
     </div>
   );
 }
