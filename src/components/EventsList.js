941c442c98b59d3e421c373796c9bc5ca47ededf
jamie7533 <lee.jamie.july@gmail.com>
2022-04-27 08:52:16 -0500

    fix search on home page

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -5,28 +5,43 @@ import Event from './Event';
 import NavBar from './NavBar';
 import SearchBar from './SearchBar';
 
-const EventsList = ({ events, searchText, userData, currentPage, filterCondition}) => {
+const EventsList = ({ events, searchText, userData, currentPage, filterCondition }) => {
   const [user] = useUserState();
 
-  const joined_condition = (event) => {return user && userData && userData[user.uid].joined_events && 
-    userData[user.uid].joined_events.includes(event.id);};
+  const joined_condition = (event) => {
+    return user && userData && userData[user.uid].joined_events &&
+      userData[user.uid].joined_events.includes(event.id);
+  };
 
-  const hosted_condition = (event) => {return user && userData && userData[user.uid].hosted_events && 
-    Object.values(userData[user.uid].hosted_events).includes(event.id);};
+  const hosted_condition = (event) => {
+    return user && userData && userData[user.uid].hosted_events &&
+      Object.values(userData[user.uid].hosted_events).includes(event.id);
+  };
 
-  const filteredEvents = Object.values(events).filter(event =>
-    String(event.title +
-    event.description +
-    event.sport +
-    event.host +
-    event.location).toLowerCase().includes(searchText.toLowerCase()))
-            .filter(currentPage === "myevents" ? 
-                    (filterCondition === "joined_events" ? joined_condition : hosted_condition ): e => true );
+  const filteredEvents = () => {
+    if (currentPage === "myevents") {
+      return Object.values(events).filter(event =>
+        String(event.title +
+          event.description +
+          event.sport +
+          event.host +
+          event.location).toLowerCase().includes(searchText.toLowerCase()))
+        .filter(currentPage === "myevents" ?
+          (filterCondition === "joined_events" ? joined_condition : hosted_condition) : e => true);
+    };
+    return Object.values(events).filter(event =>
+      String(event.title +
+        event.description +
+        event.sport +
+        event.host +
+        event.location).toLowerCase().includes(searchText.toLowerCase()));
+  }
 
   return (
     <Stack direction="vertical" gap={3}>
-    { filteredEvents.map(event => <Event key={ event.id } event={ event } events={events} userData={userData} />) }
+      {filteredEvents().map(event => <Event key={event.id} event={event} events={events} userData={userData} />)}
     </Stack>
-  )};
+  )
+};
 
 export default EventsList;
