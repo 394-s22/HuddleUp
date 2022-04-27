5a3b72e140bb9af0f16ef0dc001a39581680c554
yfarge <83893088+yfarge@users.noreply.github.com>
2022-04-26 21:20:54 -0500

    added ViewPlayers button and corresponding modal

---
+++ src/components/PlayerList.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Modal, Card, Button } from "react-bootstrap";
+
+const PlayerList = ({playerList, isDisabled}) => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+    <>
+        <Button style={{float: 'right'}} 
+                variant="primary" 
+                onClick={handleShow}
+                disabled={isDisabled}>
+            View Players
+        </Button>
+        <Modal 
+            show={show}
+            onHide={handleClose}
+            centered>
+            <Modal.Header closeButton>
+                <Modal.Title>List of Attending Players</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {playerList.length > 0 ?
+                    playerList.map((displayName, key) => {
+                    return (
+                        <Card key={key} 
+                              style={{border: 'none'}} 
+                              className="mb-3">
+                            <Card.Text>{displayName}</Card.Text>
+                        </Card>
+                    )}
+                ) : <p style={{textAlign: 'center'}}>
+                        No current players. Be the first to join!
+                    </p>
+            }
+            </Modal.Body>
+        </Modal>
+    </>
+    )   
+}
+
+export default PlayerList;
